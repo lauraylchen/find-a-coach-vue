6bc62d9f7672e33341ad3cf8009d633c9bf027b8
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-10-18 18:11:24 -0400

    Created BaseCard component and import as global component

--- src/main.js
+++ src/main.js
@@ -4,9 +4,13 @@ import router from './router';
 import store from './store/index';
 import App from './App.vue';
 
+import BaseCard from './components/UI/BaseCard.vue';
+
 const app = createApp(App);
 
 app.use(router);
 app.use(store);
+
+app.component('base-card', BaseCard);
 
 app.mount('#app');
