9f8ac132d1e3e0ce79c5decedc10f2582f8a64a2
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-10-18 18:22:53 -0400

    Cerated BaseButton component and import as global component

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from './store/index';
 import App from './App.vue';
 
 import BaseCard from './components/UI/BaseCard.vue';
+import BaseButton from './components/UI/BaseButton.vue';
 
 const app = createApp(App);
 
@@ -12,5 +13,6 @@ app.use(router);
 app.use(store);
 
 app.component('base-card', BaseCard);
+app.component('base-button', BaseButton);
 
 app.mount('#app');
