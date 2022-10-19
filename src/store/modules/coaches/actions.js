58e8b5f758c809baa0b44003763e8c7091864c56
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-10-19 17:16:50 -0400

    Using firestore to save registered coach

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,7 +1,7 @@
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
     const coachData = {
-      id: context.rootGetters.userId,
       firstName: data.first,
       lastName: data.last,
       description: data.desc,
@@ -9,6 +9,18 @@ export default {
       areas: data.areas
     };
 
-    context.commit('registerCoach', coachData);
+    const response = await fetch(`https://findacoach-app-default-rtdb.firebaseio.com/coaches/${userId}.json`, {
+      method: 'PUT',
+      body: JSON.stringify(coachData)
+    });
+
+    if (!response.ok) {
+      // error ...
+    }
+
+    context.commit('registerCoach', {
+      ...coachData,
+      id: userId
+    });
   }
 };
